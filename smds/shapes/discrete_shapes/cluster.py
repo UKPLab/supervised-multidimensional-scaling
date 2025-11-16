2e0bc0b5b236779c6098026f8efcfd3cf6228298
Jan Karas <mygnirut@gmail.com>
2025-11-16 14:54:37 +0100

    Remove setup.py as it will not longer be used. Add things to pyproject toml to enable publishing the package on PyPI. Formating code because somehow we had unformatted code on main. Pozdrowienia

--- smds/shapes/discrete_shapes/cluster.py
+++ smds/shapes/discrete_shapes/cluster.py
@@ -5,12 +5,13 @@ from smds.shapes.base_shape import BaseShape
 
 class ClusterShape(BaseShape):
     """
-        Implements the cluster-shape for categorical data.
+    Implements the cluster-shape for categorical data.
 
-        This shape models data where the only meaningful distinction is category
-        membership. The ideal distance is 0 for points within the same category
-        and 1 for points in different categories.
-        """
+    This shape models data where the only meaningful distinction is category
+    membership. The ideal distance is 0 for points within the same category
+    and 1 for points in different categories.
+    """
+
     def __init__(self) -> None:
         pass
 
@@ -28,4 +29,3 @@ class ClusterShape(BaseShape):
         distance_matrix = (y[:, None] != y[None, :]).astype(float)
 
         return distance_matrix
-
