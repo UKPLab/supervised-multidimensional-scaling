e39a327ffef9fda3f3f7dcca529bc14224d8cb7a
arwinsg <arwin.sanee@stud.tu-darmstadt.de>
2025-11-24 00:03:04 +0100

    fix typing

--- smds/stress/non_metric_stress.py
+++ smds/stress/non_metric_stress.py
@@ -1,12 +1,13 @@
 import numpy as np
 from numpy.typing import NDArray
+from scipy.spatial.distance import pdist
 from sklearn.isotonic import IsotonicRegression
 
 from smds.stress.base_stress import BaseStress
 
 
 class NonMetricStress(BaseStress):
-    """
+     """
     Notes:
     - Time complexity: ... # TODO: Decide if we want to include such things. IMO could be interesting
     - Space complexity: ...
@@ -15,7 +16,7 @@ class NonMetricStress(BaseStress):
         Smelser et al., "Normalized Stress is Not Normalized: How to
         Interpret Stress Correctly", https://arxiv.org/html/2408.07724v1
     """
-    def compute(self, D_high: NDArray[np.float64], D_low: NDArray[np.float64]) -> float:
+    def compute(self, D_high: NDArray, D_low: NDArray) -> float:
 
         ir: IsotonicRegression = IsotonicRegression(increasing=True)
         d_hat: NDArray[np.float64] = ir.fit_transform(D_high, D_low)
