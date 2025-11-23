ae5dda2c1a26baf448adab4fb08b8490bad6a3e0
arwinsg <arwin.sanee@stud.tu-darmstadt.de>
2025-11-23 19:39:40 +0100

    fix: typing

--- smds/shapes/coordinates/base_coordinates.py
+++ smds/shapes/coordinates/base_coordinates.py
@@ -1,9 +1,7 @@
 from abc import ABC, abstractmethod
 
-from smds.shapes.coordinates.cartesian_coordinates import CartesianCoordinates
-
 
 class BaseCoordinates(ABC):
     @abstractmethod
-    def to_cartesian(self) -> CartesianCoordinates:
-        raise NotImplementedError()
+    def to_cartesian(self) -> "CartesianCoordinates":
+        pass
