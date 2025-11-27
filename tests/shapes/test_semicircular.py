6dd8deca36b577de7fdda2b17a4d649e15b12884
Vinayak Joshi <vinayakjoshi@Vinayaks-MacBook-Pro.local>
2025-11-27 22:48:52 +0100

    tests added

---
+++ tests/shapes/test_semicircular.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pytest
+from smds import SupervisedMDS
+from smds.shapes.continuous_shapes.semicircular import SemicircularShape
+
+@pytest.fixture
+def engine() -> SupervisedMDS:
+    # Semicircle is 2D
+    return SupervisedMDS(n_components=2, manifold=SemicircularShape(), alpha=0.1)
+
+@pytest.fixture
+def X() -> np.ndarray:
+    return np.random.randn(100, 20)
+
+@pytest.fixture
+def y() -> np.ndarray:
+    # Normalized to [0, 1]
+    return np.random.rand(100)
+
+def test_semicircular_smoke(engine: SupervisedMDS, X: np.ndarray, y: np.ndarray) -> None:
+    engine.fit(X, y)
+    X_proj = engine.transform(X)
+    assert X_proj.shape == (100, 2)
