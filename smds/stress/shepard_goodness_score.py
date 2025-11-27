48dfece0eeada3e3732b69208cb1c5aceecb5b13
Jan Karas <mygnirut@gmail.com>
2025-11-27 09:37:53 +0100

    doc string

--- smds/stress/shepard_goodness_score.py
+++ smds/stress/shepard_goodness_score.py
@@ -6,6 +6,11 @@ from scipy.spatial.distance import pdist
 
 
 class ShepardGoodnessScore(BaseStress):
+    """
+    Reference:
+        Smelser et al., "Normalized Stress is Not Normalized: How to
+        Interpret Stress Correctly", https://arxiv.org/html/2408.07724v1
+    """
     def compute(self, X_high: NDArray[np.float64], X_low: NDArray[np.float64]) -> float:
         dist_high = pdist(X_high)
         dist_low = pdist(X_low)
