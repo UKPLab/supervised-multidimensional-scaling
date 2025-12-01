26e57438dbbcdec8949a52cd9213f2726e1843ca
Jan Karaś <90987511+KTFish@users.noreply.github.com>
2025-12-01 18:13:49 +0100

    feat: Shepard Score (#68)

--- smds/stress/shepard_goodness_score.py
+++ smds/stress/shepard_goodness_score.py
@@ -1,7 +1,20 @@
-from base_stress import BaseStress
+from scipy.stats import spearmanr
 from numpy.typing import NDArray
+import numpy as np
 
 
-class ShepardGoodnessScore(BaseStress):
-    def compute(self, X_high: NDArray, X_low: NDArray) -> float:
-        raise NotImplementedError
+def shepard_goodness_score(D_high: NDArray[np.float64], D_low: NDArray[np.float64]) -> float:
+    """
+    Compute the Shepard Goodness Score (Spearman's Rho of distance matrices).
+
+    This metric evaluates the preservation of the global structure of the data
+    by correlating the pairwise distances in the high-dimensional space with
+    the pairwise distances in the low-dimensional space.
+
+    Reference:
+        Smelser et al., "Normalized Stress is Not Normalized: How to
+        Interpret Stress Correctly", https://arxiv.org/html/2408.07724v1
+    """
+    rho, _ = spearmanr(D_high, D_low)
+
+    return rho
