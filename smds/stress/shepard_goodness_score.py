fe2cc0de3ace8c3fc0ff53666431ae0936183274
arwinsg <arwin.sanee@stud.tu-darmstadt.de>
2026-01-01 21:43:15 +0100

    fix: reset nan debugging

--- smds/stress/shepard_goodness_score.py
+++ smds/stress/shepard_goodness_score.py
@@ -29,10 +29,6 @@ def shepard_goodness_stress(d_true: NDArray[np.float64], d_pred: NDArray[np.floa
     score : float
         Spearman's rank correlation coefficient (rho).
 
-    Notes
-    -----
-    If the arrays are constant, the correlation is 0.0.
-
     References
     ----------
     - Smelser, K., Miller, J., & Kobourov, S. (2024). "Normalized Stress is Not
@@ -41,9 +37,6 @@ def shepard_goodness_stress(d_true: NDArray[np.float64], d_pred: NDArray[np.floa
     d_true = check_array(d_true, ensure_2d=False, dtype=np.float64)
     d_pred = check_array(d_pred, ensure_2d=False, dtype=np.float64)
     check_consistent_length(d_true, d_pred)
-
-    if np.std(d_true) < 1e-9 or np.std(d_pred) < 1e-9:
-        return 0.0
 
     correlation = spearmanr(d_true, d_pred)
 
