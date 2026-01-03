0667ddfdf0c195c775d20c56096036d3298ca033
Vinayak Joshi <vinayakjoshi2001@gmail.com>
2026-01-03 21:27:48 +0530

    updated continuous_shapes/__init__.py

--- smds/shapes/continuous_shapes/__init__.py
+++ smds/shapes/continuous_shapes/__init__.py
@@ -3,5 +3,6 @@ from .euclidean import EuclideanShape
 from .log_linear import LogLinearShape
 from .semicircular import SemicircularShape
 from .spiral_shape import SpiralShape
+from .torus import TorusShape
 
 __all__ = ["CircularShape", "EuclideanShape", "LogLinearShape", "SemicircularShape", "SpiralShape", "TorusShape"]
