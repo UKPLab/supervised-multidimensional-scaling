b31cb1902b377272a91362547ad99fa15680cb7e
Jan Karas <mygnirut@gmail.com>
2026-02-06 13:17:22 +0100

    xd

--- smds/__init__.py
+++ smds/__init__.py
@@ -1,12 +1,3 @@
-import warnings
-
-warnings.warn(
-    "The 'supervised-multidimensional-scaling' package on PyPI has been renamed to smds. "
-    "Please update your dependencies.",
-    DeprecationWarning,
-    stacklevel=2
-)
-
 from .smds import SupervisedMDS
 
 __all__ = ["SupervisedMDS"]
